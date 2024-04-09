2cc835334037dfaf0427aca576f0283c3a4dbaeb
Victor Okpukpan <vokpukpan@gmail.com>
2024-04-09 10:18:33 +0100

    style: import montserrat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-7xl font-xeroda">We are coming soon</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-center text-white">We are coming soon</h1>
+      <p className="text-white">This Domain Belongs to FACOSA</p>
     </main>
   );
 }
