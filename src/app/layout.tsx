e2ac9fed15ce5e1e4ef1d246520f652cc7c4788a
Victor Okpukpan <vokpukpan@gmail.com>
2024-04-09 07:52:56 +0100

    style: change background and import the xeroda font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,19 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localfont from "next/font/local";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const xeroda = localfont({
+  src: [
+    {
+      path: "../../public/xeroda-font/XerodaRegular-p7dwr.otf",
+      
+    }
+  ],
+  variable: "--font-xeroda"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={xeroda.variable}>{children}</body>
     </html>
   );
 }
