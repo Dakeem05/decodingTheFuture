c634abe62698c83a44f6cf74a00cd6330f0c321b
Victor Okpukpan <vokpukpan@gmail.com>
2024-04-10 10:41:59 +0100

    style: responsive styles for tablets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import { BsTwitterX } from "react-icons/bs";
 export default function Home() {
   return (
     <main className="max-h-screen overflow-hidden max-w-screen-lg mx-auto">
-      <header className="h-[15vh] py-3 px-3 md:px-0">
-        <img src="/assets/logo.svg" alt="Woman in Virtual Reality" className="object-cover h-14"/>
+      <header className="md:h-[10vh] py-3 px-3 md:px-0">
+        <img src="/assets/logo.svg" alt="Woman in Virtual Reality" className="object-cover h-7 md:h-14"/>
       </header>
-      <div className="flex min-h-screen md:max-h-[85vh] items-center justify-center md:justify-between">
+      <div className="flex min-h-screen lg:max-h-[90vh] items-center justify-center lg:justify-between">
         <div className="">
           <h1 className="text-white mb-5">We are coming soon</h1>
-          <form action="" className="w-[291px]">
+          <form action="" className="w-[291px] mx-auto lg:mx-0">
             <InputBox type="text" placeholder="Name" />
             <InputBox type="email" placeholder="Email Address" />
             <InputBox type="tel" placeholder="Phone Number" />
@@ -20,14 +20,14 @@ export default function Home() {
               Join Us
             </button>
           </form>
-          <div className="text-white mt-14 flex items-center space-x-5">
+          <div className="text-white mt-10 flex items-center space-x-5 w-max lg:w-full mx-auto lg:mx-0">
             <FaFacebook className="w-5 h-5 cursor-pointer" />
             <BsTwitterX className="w-5 h-5 cursor-pointer" />
             <FaInstagram className="w-5 h-5 cursor-pointer" />
             <FaLinkedin className="w-5 h-5 cursor-pointer" />
           </div>
         </div>
-        <div className="w-2/3 hidden md:block min-h-screen 2xl:flex items-center relative">
+        <div className="w-2/3 hidden lg:block min-h-screen 2xl:flex items-center relative">
           <img
             src="/assets/vr.png"
             alt="Woman in Virtual Reality"
