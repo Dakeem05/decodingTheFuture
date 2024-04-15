e986e8364b6078756eaaf40aa99f89509af18089
Victor Okpukpan <vokpukpan@gmail.com>
2024-04-15 12:55:12 +0100

    style: update landing page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,13 @@ import { BsTwitterX } from "react-icons/bs";
 export default function Home() {
   return (
     <main className="max-h-screen overflow-hidden max-w-screen-lg mx-auto">
-      <header className="md:h-[10vh] py-3 px-3 md:px-0">
-        <img src="/assets/logo.svg" alt="Woman in Virtual Reality" className="object-cover h-7 md:h-14"/>
+      <header className="md:h-[5vh] py-3 px-3 md:px-0">
+        <img src="/assets/logo.svg" alt="Woman in Virtual Reality" className="object-cover h-7 md:h-12"/>
       </header>
-      <div className="flex min-h-screen lg:max-h-[90vh] items-center justify-center lg:justify-between">
-        <div className="">
-          <h1 className="text-white mb-5">We are coming soon</h1>
+      <div className="flex min-h-screen lg:max-h-[95vh] items-center justify-center lg:justify-between">
+        <div className="px-3 md:px-0">
+          <h1 className="text-white text-center lg:text-left">We are coming soon</h1>
+          <p className="md:w-[450px] text-center lg:text-left text-white font-xeroda my-3">Decoding the Future is a 5-Day event is designed to empower aspiring tech enthusiasts and future leaders through immersive workshops and conference on AI/XR & IoT, Blockchain, Cybersecurity, Career & Finance.</p>
           <form action="" className="w-[291px] mx-auto lg:mx-0">
             <InputBox type="text" placeholder="Name" />
             <InputBox type="email" placeholder="Email Address" />
