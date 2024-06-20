bd5b03459989529570930c8a9026ff03d4e6182a
dakeem05 <edidiongsamuel14@gmail.com>
2024-06-20 08:19:16 +0100

    Fixed the alignment issh

--- src/components/Successful.tsx
+++ src/components/Successful.tsx
@@ -5,9 +5,11 @@ import Image from "next/image";
 export default function VerifyOTP() {
   return (
     <div className="w-fit mx-auto text-white">
-        <Image src="/assets/Animation - 1718865576543.gif" alt="logo" width={123} height={60} />
-        <h1>Congratulations!!</h1>
-        <h2>You have successfullly registered for Decoding the future event organized by FACOSA, the team would keep in touch with you via emails 😊😚</h2>
+        <div className="w-fit mx-auto ">
+          <Image src="/assets/Animation - 1718865576543.gif" alt="logo" width={123} height={60} />
+        </div>
+        <h1 className="text-center lg:text-left">Congratulations!!</h1>
+        <h2 className="text-center lg:text-left font-xeroda ">You have successfullly registered for Decoding the future event organized by FACOSA, the team would keep in touch with you via emails 😊😚</h2>
     </div>
   );
 }
